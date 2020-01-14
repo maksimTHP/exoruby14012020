b924014de48bf31b84181358c387a35bfe8d253f
Maxime Besse <bessemaxime@gmail.com>
2020-01-14 13:20:56 +0100

    exo 9 done

---
+++ exo_9.rb
@@ -0,0 +1,8 @@
+puts "Bonjour, quel est ton prénom ?"
+print "> "
+prenom_user = gets.chomp
+puts "Ok. Et quel est ton nom de famille ?"
+print "> "
+nom_user = gets.chomp
+
+puts "Dans ce cas, on te souhaite la bienvenue #{prenom_user} #{nom_user} ! C'est cool de te voir parmi nous !"
