0acdd40f24981fe61fcae896942498074b2ae0b4
Maxime Besse <bessemaxime@gmail.com>
2020-01-14 12:00:24 +0100

    exo7c done

---
+++ exo_7_c.rb
@@ -0,0 +1,4 @@
+user_name = gets.chomp
+puts user_name
+
+#le programme fait pareille que les autres sauf qu'il n'y a pas d'indication et que si ce n'est pas affiché, en tant qu'user on ne sait pas quoi faire. Lorsque on run le programme, visuellement, rien ne se passe, alors que le programme demande juste que l'user mette un string.
