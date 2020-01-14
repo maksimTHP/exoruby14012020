bea6d2bcc6bbcc99d8480cc8a3eb40139091d15e
AnouarLdn <anouar.haidary@gmail.com>
2020-01-14 11:57:36 +0100

    Exo 7 done

---
+++ exo_07_b.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+#le programme demande a l'utilisateur d'inserer un string (un chevron apparait pour que l'utilisateur sache qu'il faut taper quelque chose)
