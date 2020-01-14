6a13452ed4dc5c7aa77bec318f2e5bc4370d08b6
Maxime Besse <bessemaxime@gmail.com>
2020-01-14 17:10:33 +0100

    exo 17 à jour et fonctionnel

--- exo_17.rb
+++ exo_17.rb
@@ -9,12 +9,11 @@ age = 0
 half_age = age/2
 
 while yearofbirth <= thisyear
-  puts "En #{yearofbirth} tu avais #{age} ans!"
-  yearofbirth = yearofbirth +1
-    if (age/2 == half_age && age%2 == 0) #
-    then puts "En #{yearofbirth}, à #{age} ans, tu avais le double de l'âge que tu avais il y a #{half_age} ans!"
-  else #On ne met rien on veut pas de sinon
-    end
-  age = age +1
+  puts "En #{yearofbirth} tu avais #{age} ans !"
+  if (age/2 == half_age && age%2 == 0)
+  then puts "En #{yearofbirth}, à #{age} ans, tu avais le double de l'âge que tu avais il y a #{half_age} ans!"
+  end
+  yearofbirth +=1
+  age +=1
   half_age = age/2
 end
