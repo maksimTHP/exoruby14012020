bd6cc1f5bede5934a07466a2a2bf09ec3e5d748c
Maxime Besse <bessemaxime@gmail.com>
2020-01-14 11:08:22 +0100

    Exo 5 Done

---
+++ exo_05.rb
@@ -0,0 +1,41 @@
+#On retourne un string
+puts "On va compter le nombre d'heures de travail à THP"
+#On retourne travail et on affiche le résultat du calcul 10 x 5 X 11 dans la valeur retournée
+puts "Travail : #{10 * 5 * 11}"
+#On retourne travail et on affiche le résultat du calcul 10 x 5 X 11 x 60 dans la valeur retournée
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+#On retourne un string
+puts "Et en secondes ?"
+
+#On retourne un string, pas de calcul car par de crochets, tout est pris sous forme de string
+puts "Est ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+#On affiche le résultat booléen de l'inégalité. 3 +2 n'est pas inférieur à 5 - 7 alors il renvoie False et il l'affiche à l'écran grâce au puts
+puts 3 + 2 < 5 - 7
+
+#L18/19 On retourne un string et dans cette même string on fait un calcul entre les crochets.
+puts "ca fait combien 3 + 2 ? #{3 + 2}"
+puts "Ca fait combien 5 - 7 ? #{5 - 7}"
+
+# L22/23 On retourne un string
+puts "Ok, c'est faux alors !"
+puts "C'est drôle ça, faisons-en plus :"
+
+#On retourne un string puis on affiche le résultat booléen de l'inégalité. 5 est supérieur -2 alors il renvoie True et il l'affiche à l'écran grâce au puts
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+
+#On retourne un string puis on affiche le résultat booléen de l'inégalité. 5 est supérieur OU égal -2 alors il renvoie True et il l'affiche à l'écran grâce au puts
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#On retourne un string puis on affiche le résultat booléen de l'inégalité. 5 est inférieur OU égal à -2 alors il renvoie False et il l'affiche à l'écran grâce au puts
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+
+
+
+
+
+
+
+
+
+#PREMIÈRE RÉPONSE #{} Sert à faire des calculs ou à renvoyer un true or false (booléen) sur une inégalité
